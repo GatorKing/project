1c314be6d1dfef7e4d7b40a9b3e98d2d6fcc4e9a
Vagunda <vagundamatusgmail.com>
2018-05-11 20:41:32 +0200

    pridany bootstrap

--- projekt/application/config/autoload.php
+++ projekt/application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
@@ -103,6 +103,7 @@ $autoload['helper'] = array();
 | config files.  Otherwise, leave it blank.
 |
 */
+
 $autoload['config'] = array();
 
 /*
--- projekt/application/views/templates/template/footer.php
+++ projekt/application/views/templates/template/footer.php
@@ -1,8 +1,11 @@
-/**
- * Created by PhpStorm.
- * User: Uzivatel
- * Date: 11. 5. 2018
- * Time: 16:27
- */
-<script src="<?php echo
-base_url();?>assets/js/bootstrap.min.js"><</script>
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="https://getbootstrap.com/docs/3.3/dist/js/bootstrap.min.js"></script>
+<script src="https://getbootstrap.com/docs/3.3/assets/js/docs.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="https://getbootstrap.com/docs/3.3/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
